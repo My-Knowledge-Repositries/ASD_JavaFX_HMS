bb15cc3aea6640c73dcdf497d43124a9cd56dd2f
oshadaera68 <mindartlk010@gmail.com>
2023-03-15 23:55:33 +0530

    updated content

--- src/com/developersstack/medex/controller/LoginFormController.java
+++ src/com/developersstack/medex/controller/LoginFormController.java
@@ -7,12 +7,20 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public JFXTextField txtEmail;
     public JFXPasswordField txtPassword;
     public JFXRadioButton rBtnDoctor;
+    public AnchorPane loginContext;
 
     public void signInOnAction(ActionEvent actionEvent) {
         String email = txtEmail.getText();
@@ -40,7 +48,8 @@ public class LoginFormController {
         new Alert(Alert.AlertType.WARNING, String.format("we can't find an email (%s)", email)).show();
     }
 
-    public void createAnAccountOnAction(ActionEvent actionEvent) {
-
+    public void createAnAccountOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) loginContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/SignUpForm.fxml"))));
     }
 }
