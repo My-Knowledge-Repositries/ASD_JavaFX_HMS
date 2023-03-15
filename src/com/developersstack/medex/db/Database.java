3ce6ef404bb297b17100496a4ef5795baeac75f5
oshadaera68 <mindartlk010@gmail.com>
2023-03-15 22:35:09 +0530

    added user and doctor in database class

--- src/com/developersstack/medex/db/Database.java
+++ src/com/developersstack/medex/db/Database.java
@@ -1,9 +1,17 @@
 package com.developersstack.medex.db;
 
 import com.developersstack.medex.dto.UserDto;
+import com.developersstack.medex.enums.AccountType;
 
 import java.util.ArrayList;
 
 public class Database {
     public static ArrayList<UserDto> userTable = new ArrayList();
+
+    static {
+        //--------
+        userTable.add(new UserDto("Hasika","Sadaruwan","hasika@gmail.com","1234", AccountType.PATIENT));
+        userTable.add(new UserDto("Imesh","hirushan","imeHiru@gmail.com","1234", AccountType.DOCTOR));
+        //--------
+    }
 }
