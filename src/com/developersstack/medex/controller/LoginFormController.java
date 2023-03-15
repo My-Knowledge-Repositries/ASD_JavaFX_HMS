db4e2e876740253f06bd6c4f531b0b8984c719b4
oshadaera68 <mindartlk010@gmail.com>
2023-03-15 22:10:58 +0530

    finished login form

--- src/com/developersstack/medex/controller/LoginFormController.java
+++ src/com/developersstack/medex/controller/LoginFormController.java
@@ -1,4 +1,20 @@
 package com.developersstack.medex.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+    public JFXRadioButton rBtnDoctor;
+
+    public void signInOnAction(ActionEvent actionEvent) {
+
+    }
+
+    public void createAnAccountOnAction(ActionEvent actionEvent) {
+
+    }
 }
