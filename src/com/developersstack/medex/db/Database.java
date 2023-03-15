36c2332248e84193cebb0ebb23885c83169e58d1
oshadaera68 <mindartlk010@gmail.com>
2023-03-15 22:30:56 +0530

    added dto class

---
+++ src/com/developersstack/medex/db/Database.java
@@ -0,0 +1,9 @@
+package com.developersstack.medex.db;
+
+import com.developersstack.medex.dto.UserDto;
+
+import java.util.ArrayList;
+
+public class Database {
+    public static ArrayList<UserDto> userTable = new ArrayList();
+}
