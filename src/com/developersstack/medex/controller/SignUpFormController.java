16a2e94e2c4a8f691cff23c26ff7c9cf4727ef11
oshadaera68 <mindartlk010@gmail.com>
2023-03-16 08:22:22 +0530

    updated signin form

--- src/com/developersstack/medex/controller/SignUpFormController.java
+++ src/com/developersstack/medex/controller/SignUpFormController.java
@@ -1,4 +1,22 @@
 package com.developersstack.medex.controller;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXRadioButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class SignUpFormController {
+    public AnchorPane signUpContext;
+    public JFXTextField txtFirstName;
+    public JFXTextField txtLastName;
+    public JFXTextField txtEmail;
+    public JFXPasswordField txtPassword;
+    public JFXRadioButton rBtnDoctor;
+
+    public void signInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void alreadyHaveAnAccountOnAction(ActionEvent actionEvent) {
+    }
 }
